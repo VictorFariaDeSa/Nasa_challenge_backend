761ba49b43c98b586ab944e5d2f5457cfd050834
VictorFariadeSa <Savictor3963@gmail.com>
2025-10-04 02:44:24 -0300

    feat: category table creation

--- database/seeds/Create_tables.sql
+++ database/seeds/Create_tables.sql
@@ -3,3 +3,9 @@ CREATE TABLE IF NOT EXISTS topics (
     name VARCHAR(50),
     summary TEXT
 );
+
+CREATE TABLE IF NOT EXISTS category (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50), 
+    summary TEXT
+);
