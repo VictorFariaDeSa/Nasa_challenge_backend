6008d94f39a18734dfd97824baba27451e878cce
VictorFariadeSa <Savictor3963@gmail.com>
2025-10-04 02:47:04 -0300

    feat: articles table creation

--- database/seeds/Create_tables.sql
+++ database/seeds/Create_tables.sql
@@ -4,8 +4,16 @@ CREATE TABLE IF NOT EXISTS topics (
     summary TEXT
 );
 
-CREATE TABLE IF NOT EXISTS category (
+CREATE TABLE IF NOT EXISTS categories (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50), 
     summary TEXT
+);
+
+CREATE TABLE IF NOT EXISTS articles (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50), 
+    summary TEXT,
+    link VARCHAR(100),
+    publish_date DATE
 );
